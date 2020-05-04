9f0c5da3b849fb439ab4763774cdcf99f04bb5b1
Roni <roni1268888@example.com>
2020-05-03 22:53:33 -0400

    mdn tut - part 5: displaying library data

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 var Schema = mongoose.Schema;
 var AuthorSchema = new Schema(
     {
@@ -21,7 +22,7 @@ AuthorSchema.virtual('name').get(function()
         fullname = '';
     }
     return fullname;
-})
+});
 
 AuthorSchema.virtual('lifespan').get(function()
 {
@@ -29,8 +30,20 @@ AuthorSchema.virtual('lifespan').get(function()
 });
 
 AuthorSchema.virtual('url').get(function(){
-    return '/catalog/author/' + this._id
+    return '/catalog/author/' + this._id;
 });
+
+AuthorSchema.virtual('date_of_birth_formatted')
+.get(function(){
+    return this.date_of_birth ? moment(this.date_of_birth).format('MMMM Do, YYYY') : "";
+});
+
+AuthorSchema.virtual('date_of_death_formatted')
+.get(function(){
+    return this.date_of_death ? moment(this.date_of_death).format('MMMM Do, YYYY') : "";
+});
+
+
 
 module.exports = mongoose.model('Author', AuthorSchema);
 
--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 var Schema = mongoose.Schema;
+
 var BookInstanceSchema = new Schema(
     {
         book: {type: Schema.Types.ObjectId, ref: 'Book', required: true},
@@ -13,6 +15,12 @@ BookInstanceSchema
 .virtual('url')
 .get(function(){
     return '/catalog/bookinstance/' + this._id;
+});
+
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function(){
+    return moment(this.due_back).format('MMMM Do, YYYY');
 });
 
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
